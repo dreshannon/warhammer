0ea163c4804efb25e4fcb1bf0bd00913e6c8fa07
Dre Shannon <dshannon@mail.missouri.edu>
2019-02-22 14:46:19 -0600

    Updated Home page and deployment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld class="text-center" msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div>
+            <h1>Welcome to the Warhammer Companion Application!</h1>
+            <div class="row">
+                <div class="col-6">
+                    <h3>Current completed pages:</h3>
+                    <ul>
+                        <li>Login</li>
+                        <li>Compendium - Armies</li>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <h3>Pages still in progress</h3>
+                    <ul>
+                        <li>Home</li>
+                        <li>Basic Rules</li>
+                        <li>Kill Team Rules</li>
+                        <li>Compendium - Weapons</li>
+                        <li>My Armies</li>
+                        <li>Player Lists</li>
+                    </ul>
+                </div>
+            </div>
+            <h3>Known Errors</h3>
+            <p>On the <strong>Compendium - Armies</strong> page, there is an error with adding/editing keywords.</p>
+            <h3>Note to all</h3>
+            <p>If you find or create a logo/background/color scheme you think would go good here, send to over!</p>
+            <p>If you see any errors or something that just doesn't look right, let me know!</p>
+        </div>
+        <div class="text-center">
+            <img alt="Vue logo" src="../assets/logo.png">
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home'
 }
 </script>
